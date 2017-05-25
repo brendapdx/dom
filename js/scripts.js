5e8c68765bda734d7e85c3fae98c3f501f77993b
Brenda <bfarrier@mail.com>
2017-05-24 18:59:12 -0700

    add cat-dog html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-    $("ul").prepend("<li>Hello!</li>");
-  });
+  $("ul#user").prepend("<li>Hello!</li>");
+  $("ul#webpage").prepend("<li>Why hello there!</li>");
+  $('li').css('background-color', 'green');
+});
 
   $("button#goodbye").click(function() {
     $("ul").prepend("<li>Goodbye!</li>");
@@ -10,4 +12,8 @@ $(document).ready(function() {
   $("button#stop").click(function() {
     $("ul").prepend("<li>Stop copying me!</li>");
   });
+});
+
+$("button#hello").click(function() {
+$("p#salad").prepend("<li>Hello!</li>");
 });
